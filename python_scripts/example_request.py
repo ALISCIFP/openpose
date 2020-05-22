9b716719a5ed607ade442afade98e32ab310d9b8
Zengming(Zack) Shen <szm0219@gmail.com>
2020-05-22 19:01:52 -0400

    test

--- python_scripts/example_request.py
+++ python_scripts/example_request.py
@@ -14,6 +14,6 @@ def base64_from_fp(fp_img):
 fp_img = "/export/zs/s2g/oliver_face70_AJ/test_img/Alex_Jones_-_Last_Week_Tonight_with_John_Oliver_HBO-WyGq6cjcc3Q_29_605_0_14_08.png"
 base64_bytes = base64_from_fp(fp_img)
 r = requests.post("http://localhost:3000/openpose", data=base64_bytes)
-print(type(r.content["people"]))
-print(type(r.content["people"]))
+print(type(r.content[0]))
+print(type(r.content[1]))
 print(r.content)
