eb5027ecdf0a631bb7a9ec01314e4d6a55e99abe
Zengming(Zack) Shen <szm0219@gmail.com>
2020-05-22 21:43:24 -0400

    test

--- python_scripts/example_request.py
+++ python_scripts/example_request.py
@@ -1,5 +1,5 @@
 import base64
-
+import json
 import requests
 
 
@@ -11,9 +11,12 @@ def base64_from_fp(fp_img):
     return b64_bytes
 
 
-fp_img = "/export/zs/s2g/oliver_face70_AJ/test_img/Alex_Jones_-_Last_Week_Tonight_with_John_Oliver_HBO-WyGq6cjcc3Q_29_605_0_14_08.png"
+fp_img = "./zs/s2g/oliver_face70_AJ/test_img/Alex_Jones_-_Last_Week_Tonight_with_John_Oliver_HBO-WyGq6cjcc3Q_29_605_0_14_08.png"
 base64_bytes = base64_from_fp(fp_img)
 r = requests.post("http://localhost:3000/openpose", data=base64_bytes)
-print(type(r.content[0]))
-print(type(r.content[1]))
-print(r.content[0])
+print(r.content)
+json_object = json.dumps(r.content)
+
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
